59ed8c23b929a9faef5f499241fe5928ea0c84de
sudetan <73983505+sudetan@users.noreply.github.com>
2021-02-08 13:16:55 +0300

    ui changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,68 +4,147 @@ import Swiper from 'react-native-swiper'
 
 
 export default class Homepage extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                    <Image
-                                        source={require('../../assets/anasayfa.png')}
-                        resizeMode="contain"
-                        style={styles.image}
-                    ></Image>
-                    <Text style={styles.text}>Welcome to the food paradise</Text>
-                    <Swiper style={styles.wrapper} showsButtons={true}>
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.shadow}>
+
+            <Image
+              source={require('../../assets/anasayfa.png')}
+              resizeMode="contain"
+
+              style={styles.image}
+            ></Image>         
+            <Text style={styles.entryText}>Hello,Sude!</Text>
+
+          </View>
+     
+          <Text style={styles.baseText}>Welcome to the food<Text
+            style={styles.innerText}> paradise</Text>
+          </Text>
         </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
+
+
+        <View style={styles.swiperContainer}>
+          <Swiper style={styles.wrapper} showsButtons={true}>
+            <View style={styles.slide1}>
+              <Text style={styles.text}>Öne Çıkanlar</Text>
+            </View>
+            <View style={styles.slide2}>
+              <Text style={styles.text}></Text>
+            </View>
+            <View style={styles.slide3}>
+              <Text style={styles.text}></Text>
+            </View>
+          </Swiper>
         </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
-        </View>
-      </Swiper>
-                </View>
-            
-        )
-        }
-    }
+      </View>
+
+    )
+  }
+}
 
 
 
-      
-      
-  
+
+
+
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "space-between",
-      backgroundColor: '#D14F15'
-    },
-    
-    
-    image: {
-      top: 13,
-      width:145,
-      height: 190,
-      position: "absolute",
-      left: 0
-    },
-    text: {
-      top: 88,
-      left: 164,
-      position: "absolute",
-      color: "#121212",
-      fontSize: 16,
-      fontWeight:'bold'
-      
-    },
-    wrapper: {
-      height:200
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 25,
+    paddingTop: 75
 
+
+  },
+  swiperContainer: {
+    height: 230,
+    top: 175
+
+
+
+
+
+
+
+  },
+  shadow: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
     },
-slide1: {
+    shadowOpacity: 0.39,
+    shadowRadius: 8.30,
+    borderRadius:45,
+
+    elevation: 20,
+    width: 100,
+    height: 100,
+    backgroundColor: 'white',
+    flexDirection:'row',
+    alignItems:'center',
+    
+
+  },
+  header: {
+
+
+
+
+
+
+  },
+
+
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius:25,
+
+
+
+
+
+
+
+
+
+
+
+  },
+  text: {
+    color: "#121212",
+    fontWeight: 'bold',
+    elevation: 20
+
+
+  },
+  entryText: {
+    fontWeight:'bold',
+    fontSize:18,
+    color:'rgba(0,0,0,0.4)',
+    left:65
+  },
+  baseText: {
+    fontWeight: 'bold',
+    top:15,
+    fontSize: 30
+
+  },
+  innerText: {
+    color: 'red'
+  },
+
+
+  wrapper: {
+
+  },
+  slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -76,18 +155,14 @@ slide1: {
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#97CAE5'
-},
-slide3: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#92BBD9'
-},
-text: {
-  color: '#fff',
-  fontSize: 30,
-  fontWeight: 'bold'
-}
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
 
-   
+
+
 })
