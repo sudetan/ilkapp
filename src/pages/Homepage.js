94c92528fbf8e78d4b8c897318330ee2eb8ecec3
bariisarslans <bariisarslans@hotmail.com>
2021-02-15 17:37:51 +0300

    description

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,7 +3,8 @@ import { Text, View, StyleSheet, Image } from 'react-native'
 import Swiper from 'react-native-swiper'
 import { FontAwesome5 } from '@expo/vector-icons';
 import { Actions } from 'react-native-router-flux';
-//
+// asdfgasga
+// asfdggsagd
 
 export default class Homepage extends Component {
   render() {
